7c03b223b2c8902f92c64fdd5aceebec33bca1ab
Dorian Vallecillo C <dorianivc1@gmail.com>
2021-07-11 18:31:02 -0600

    Labels implementados

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const app = express()
 const port =  process.env.PORT || 3000
 
 const client = new appConfig.AppConfigurationClient(process.env.AppConfigurationConnectionString);
-let connectionString=  client.getConfigurationSetting({ key: "mongo-endpoint", label: "local" });
+let connectionString=  client.getConfigurationSetting({ key: "mongo-endpoint", label: process.env.LABEL});
 let mongoUrl;
 
 app.get('/', (req, res) => {
-  connectionString=  client.getConfigurationSetting({ key: "mongo-endpoint", label: "cloud" });
+  connectionString=  client.getConfigurationSetting({ key: "mongo-endpoint", label: process.env.LABEL });
   connectionString.then(body=>{ mongoUrl=body});
   setTimeout(() => { /*console.log("connection string " + mongoUrl.value);*/ console.log('Secreto Obtenido') }, 300);
   MongoClient.connect(mongoUrl.value, { useNewUrlParser: true }, (err, db) => {
     if (err) {
       res.status(500).send('💥 Error con la conexion a la base de datos. ¿Es el App Config Secret correcto? 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB mongo de Dorian ! 😎');
+      res.send('Me conecté a la DB mongo de Dorian usando el Label: ' + process.env.LABEL + '  del App Configuration ! 😎');
       db.close();
     }
   });
